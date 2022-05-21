59a5c43985387150dac9e99dfbe2852bca56bb5d
darrenjhsu <hsudj@ornl.gov>
2022-05-20 23:10:08 -0400

    Final adjustment for traj2xyz.py

--- traj2xyz.py
+++ traj2xyz.py
@@ -2,6 +2,7 @@
 import mdtraj
 import sys
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--traj', type=str, help='File name of the pdb or dcd or xyz')
@@ -21,9 +22,21 @@ mol = mol.atom_slice(mol_sel, inplace=True)
 
 Element = [x.element.atomic_number for x in mol.top.atoms]
 
+print(f'Trajectory: {args.traj}')
+print(f'Topology: {args.top}')
+print(f'Output: {args.out}')
+print(f'Selection: {args.selection}')
+print(f'Number of frames: {len(mol)}')
+
+t0 = time.time()
+t1 = time.time()
 
 with open(args.out, 'w') as f:
     for idx, frame in enumerate(mol.xyz):
-        f.write(f'{len(Element)}\n{idx} frames\n')
+        t2 = time.time()
+        if t2 - t1 > 5: 
+            print(f'{t2 - t0} seconds, at frame {idx}')
+            t1 = t2
+        f.write(f'{len(Element)}\n{len(mol)} frames\n')
         for ele, coor in zip(Element, frame):
             f.write(f'{ele} {coor[0]*10:.3f} {coor[1]*10:.3f} {coor[2]*10:.3f}\n')
